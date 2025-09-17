b5f9e5b55c1ff1273be7b3890e92a2210dee5c53
Weng Wong (George) Hum <124537582+wengwongg@users.noreply.github.com>
2025-09-17 19:00:47 +0800

    equipment: added QA equipment

--- app/src/app/equipment/page.tsx
+++ app/src/app/equipment/page.tsx
@@ -4,10 +4,10 @@ import SectionHeading from "@/components/layout/section/section-heading";
 import SectionLabel from "@/components/layout/section/section-label";
 import GeneralContact from "@/components/pages/sections/general-contact";
 import { CONTENT_LAYOUT, SPACE_BETWEEN_SECTIONS } from "@/lib/constants/styles";
-import { Anvil, Axe } from "lucide-react";
+import { Anvil, Axe, Microscope } from "lucide-react";
 import Image from "next/image";
 
-const equipment = [
+const mainEquipment = [
   {
     heading: (
       <div className="flex items-center gap-3 mb-6 flex-wrap">
@@ -52,6 +52,37 @@ const equipment = [
   },
 ];
 
+const qualityAssuranceEquipment = [
+  {
+    heading: (
+      <div className="flex items-center gap-3 mb-6 flex-wrap">
+        <ContentHeading>Coordinate measuring machine</ContentHeading>
+        <span className="bg-primary text-background font-bold px-3 py-1 rounded-md text-sm">
+          [WENZEL] XO87-1000
+        </span>
+      </div>
+    ),
+    description:
+      "Delivers volumetric measurement uncertainty of just 1.6 + L/350 microns (μm), backed by air-bearing guides, a granite structure, and flexible sensor options for complex components — ensuring precise quality control and verification of tight tolerances across a wide range of part sizes.",
+    image: "/images/equipment/coordinate-measuring-machine.png",
+    imageAlt: "Coordinate Measuring Machine",
+  },
+  {
+    heading: (
+      <div className="flex items-center gap-3 mb-6 flex-wrap">
+        <ContentHeading>Multisensor measurement system</ContentHeading>
+        <span className="bg-primary text-background font-bold px-3 py-1 rounded-md text-sm">
+          LM-X100TL
+        </span>
+      </div>
+    ),
+    description:
+      "Offers accuracy up to ±0.7 microns (μm) and repeatability down to ±0.1 microns (μm), ensuring reliable precision tooling and quality assurance for every component we produce.",
+    image: "/images/equipment/multisensor-measurement-system.png",
+    imageAlt: "Multisensor Measurement System",
+  },
+];
+
 export default function EquipmentPage() {
   return (
     <div className={SPACE_BETWEEN_SECTIONS}>
@@ -73,7 +104,7 @@ export default function EquipmentPage() {
             </p>
           </FadeOnScroll>
           <div className="grid gap-6 lg:grid-cols-2">
-            {equipment.map((item, index) => (
+            {mainEquipment.map((item, index) => (
               <FadeOnScroll
                 key={index}
                 className="rounded bg-white border-neutral border p-6 flex flex-col items-center gap-9 sm:flex-row"
@@ -96,6 +127,46 @@ export default function EquipmentPage() {
           </div>
         </div>
       </div>
+
+      <div className={CONTENT_LAYOUT}>
+        <div className="mb-9">
+          <SectionLabel className="mb-3" icon={Microscope}>
+            Quality assurnace
+          </SectionLabel>
+          <SectionHeading className="mb-6">
+            <span className="text-primary">Maintaining</span> the{" "}
+            <span className="text-primary">highest standards.</span>
+          </SectionHeading>
+          <p>
+            On top of the main equipment above, we also have specific machinery
+            that help us to deliver outputs that are of the highest quality in
+            the industry.
+          </p>
+        </div>
+        <div className="grid gap-6 lg:grid-cols-2">
+          {qualityAssuranceEquipment.map((item, index) => (
+            <FadeOnScroll
+              key={index}
+              className="rounded bg-white border-neutral border p-6 flex flex-col items-center gap-9 sm:flex-row"
+            >
+              <Image
+                src={item.image}
+                alt={item.imageAlt}
+                width={1000}
+                height={1000}
+                className="w-full max-w-[200px] sm:w-[120px]"
+              />
+              <div>
+                {/* Custom heading */}
+                {item.heading}
+
+                <p>{item.description}</p>
+              </div>
+            </FadeOnScroll>
+          ))}
+        </div>
+      </div>
+
       <div className={CONTENT_LAYOUT}>
         <FadeOnScroll>
           <SectionLabel icon={Axe} className="mb-3">
@@ -108,9 +179,12 @@ export default function EquipmentPage() {
         </FadeOnScroll>
         <FadeOnScroll>
           <p className="mb-3">
-            We also have a full range of supporting equipment, including
-            grinding and milling machines, laser engraving, abrasive blasting
-            machines, and other precision tools.
+            We also have a full range of supporting equipment, including{" "}
+            <span className="font-bold">
+              grinding and milling machines, laser engraving, abrasive blasting
+              machines,
+            </span>{" "}
+            and other precision tools.
           </p>
           <p>
             These together ensure smooth production and maintain the highest
@@ -119,6 +193,7 @@ export default function EquipmentPage() {
           </p>
         </FadeOnScroll>
       </div>
+
       <GeneralContact />
     </div>
   );
