dd537cefd7f390d5e103111ea2e8047de5c55420
Weng Wong (George) Hum <124537582+wengwongg@users.noreply.github.com>
2025-12-06 18:53:22 +0000

    upped exposure and updated images

--- app/src/components/pages/work/material-list.tsx
+++ app/src/components/pages/work/material-list.tsx
@@ -4,33 +4,33 @@ import Modal from "@/components/ui/modal";
 import { useState } from "react";
 
 export default function MaterialList() {
-  const [isOpen, setIsOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
 
-  return (
-    <>
-      <p
-        className="text-primary underline inline-block cursor-pointer"
-        onClick={() => setIsOpen(true)}
-      >
-        comprehensive list
-      </p>
-      <Modal
-        isOpen={isOpen}
-        onClose={() => setIsOpen(false)}
-        title="Material list"
-        size="md"
-      >
-        <ul className="flex flex-col gap-3">
-          <li>ULTEM™</li>
-          <li>PEEK</li>
-          <li>Torlon®</li>
-          <li>ESD Polycarbonate</li>
-          <li>Teflon</li>
-          <li>Vespel®</li>
-          <li>Rexolite®</li>
-          <li>C-STOCK AK / AK-500</li>
-        </ul>
-      </Modal>
-    </>
-  );
+    return (
+        <>
+            <span
+                className="text-primary underline inline-block cursor-pointer"
+                onClick={() => setIsOpen(true)}
+            >
+                comprehensive list
+            </span>
+            <Modal
+                isOpen={isOpen}
+                onClose={() => setIsOpen(false)}
+                title="Material list"
+                size="md"
+            >
+                <ul className="flex flex-col gap-3">
+                    <li>ULTEM™</li>
+                    <li>PEEK</li>
+                    <li>Torlon®</li>
+                    <li>ESD Polycarbonate</li>
+                    <li>Teflon</li>
+                    <li>Vespel®</li>
+                    <li>Rexolite®</li>
+                    <li>C-STOCK AK / AK-500</li>
+                </ul>
+            </Modal>
+        </>
+    );
 }
