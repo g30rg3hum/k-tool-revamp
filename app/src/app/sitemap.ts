9824eae7c4886fba638bace1fd148741c4bdf512
Darren Tan <darrentan@Darrens-MacBook-Pro-2.local>
2026-04-17 16:30:09 +0800

    Add sustainability page to sitemap

--- app/src/app/sitemap.ts
+++ app/src/app/sitemap.ts
@@ -67,6 +67,12 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       priority: 0.6,
     },
     {
+      url: "https://www.ktoolengineering.com/sustainability",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.6,
+    },
+    {
       url: "https://www.ktoolengineering.com/blog",
       lastModified: new Date(),
       changeFrequency: "weekly",
